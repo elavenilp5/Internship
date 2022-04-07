2875a74916c5c05b94f9c1a84a77d4a0b4466780
elavenilp5 <elavenil2001@gmail.com>
2022-04-07 09:51:14 +0530

    css updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import Sidebar from "./Sidebar"
 import Tile from "./Tile"
 import SmallTileWrapper from "./SmallTileWrapper"
 import Footer from "./Footer"
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown"
 
 function Home() {
   return (
@@ -11,7 +12,18 @@ function Home() {
       <div className="wrapper">
         <div className="navwrapper">
           <div id="head1">
-            <h1 className="heading1">BOSCH</h1>
+            {" "}
+            <img src="Bosch_logo.png" alt="logo" className="imgdiv" />
+            {/* <h1 className="heading1">BOSCH</h1>{" "} */}
+          </div>
+          <div className="nav">
+            <KeyboardDoubleArrowDownIcon
+              sx={{
+                fontSize: "60px",
+                color: "white",
+                ml: 20,
+              }}
+            />
           </div>
           <div id="head2">
             <h1 className="heading2"> EHB PROCESS COMPASS</h1>
